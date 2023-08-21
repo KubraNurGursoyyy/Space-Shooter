cdbbf1b58a425ee8a1835d27b2a3fe7df1a74fff
Kübra Nur Gürsoy <55765380+KubraNurGursoyyy@users.noreply.github.com>
2023-08-21 14:10:22 +0300

    space-shooter

---
+++ Space-Shooter/Assets/Scripts/RandomRotater.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomRotater : MonoBehaviour
+{
+    Rigidbody pyhsic;
+
+    public int speed;
+
+    void Start()
+    {
+        pyhsic = GetComponent<Rigidbody>();
+
+        pyhsic.angularVelocity = Random.insideUnitSphere * speed;
+    }
+
+}
